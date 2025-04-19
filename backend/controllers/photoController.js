e1f98da376d748ddbc0d12d765de736478289599
Maksim3l <loknarmaksim@gmail.com>
2025-04-19 20:20:41 +0200

    REAC-1 initialized the project from sample
    initialized the project from the sample of it we were given

---
+++ backend/controllers/photoController.js
@@ -0,0 +1,142 @@
+var PhotoModel = require('../models/photoModel.js');
+
+/**
+ * photoController.js
+ *
+ * @description :: Server-side logic for managing photos.
+ */
+module.exports = {
+
+    /**
+     * photoController.list()
+     */
+    list: function (req, res) {
+        PhotoModel.find()
+        .populate('postedBy')
+        .sort({postedOn: -1})  
+        .exec(function (err, photos) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting photo.',
+                    error: err
+                });
+            }
+            var data = [];
+            data.photos = photos;
+            //return res.render('photo/list', data);
+            return res.json(photos);
+        });
+    },
+
+    /**
+     * photoController.show()
+     */
+    show: function (req, res) {
+        var id = req.params.id;
+
+        PhotoModel.findOne({_id: id}, function (err, photo) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting photo.',
+                    error: err
+                });
+            }
+
+            if (!photo) {
+                return res.status(404).json({
+                    message: 'No such photo'
+                });
+            }
+
+            return res.json(photo);
+        });
+    },
+
+    /**
+     * photoController.create()
+     */
+    create: function (req, res) {
+        var photo = new PhotoModel({
+		    title : req.body.title,
+            comment: req.body.comment,
+			path : "/images/"+req.file.filename,
+			postedBy : req.session.userId,
+			views : 0,
+			likes : 0
+        });
+
+        photo.save(function (err, photo) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when creating photo',
+                    error: err
+                });
+            }
+
+            return res.status(201).json(photo);
+            //return res.redirect('/photos');
+        });
+    },
+
+    /**
+     * photoController.update()
+     */
+    update: function (req, res) {
+        var id = req.params.id;
+
+        PhotoModel.findOne({_id: id}, function (err, photo) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting photo',
+                    error: err
+                });
+            }
+
+            if (!photo) {
+                return res.status(404).json({
+                    message: 'No such photo'
+                });
+            }
+
+            photo.title = req.body.title ? req.body.title : photo.title;
+            photo.comment = req.body.comment ? req.body.comment : photo.comment;
+			photo.path = req.body.path ? req.body.path : photo.path;
+			photo.postedBy = req.body.postedBy ? req.body.postedBy : photo.postedBy;
+			photo.views = req.body.views ? req.body.views : photo.views;
+			photo.likes = req.body.likes ? req.body.likes : photo.likes;
+			
+            photo.save(function (err, photo) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when updating photo.',
+                        error: err
+                    });
+                }
+
+                return res.json(photo);
+            });
+        });
+    },
+
+    /**
+     * photoController.remove()
+     */
+    remove: function (req, res) {
+        var id = req.params.id;
+
+        PhotoModel.findByIdAndRemove(id, function (err, photo) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when deleting the photo.',
+                    error: err
+                });
+            }
+
+            return res.status(204).json();
+        });
+    },
+
+    publish: function(req, res){
+        return res.render('photo/publish');
+    }
+};
---
+++ backend/models/photoModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var photoSchema = new Schema({
+	'title' : String,
+	'comment' : String,
+	'path' : String,
+	'postedBy' : {
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'user'
+	},
+	postedOn: {
+		type: Date,
+		default: Date.now 
+	  },
+	'views' : Number,
+	'likes' : Number
+});
+
+module.exports = mongoose.model('photo', photoSchema);
